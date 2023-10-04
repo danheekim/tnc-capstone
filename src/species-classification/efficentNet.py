00e282341f5f365f4f936407ccd2f3c37195fbd4
Isha <ishavaish@g.harvard.edu>
2023-10-04 00:09:36 -0400

    fixed cropping script, added get labels script, base script for efficientNet

--- src/species-classification/efficentNet.py
+++ src/species-classification/efficentNet.py
@@ -1,70 +1,111 @@
+# script for training efficientNet
+
 from google.cloud import storage
 import pandas as pd
 import numpy as np
 import tensorflow as tf
 import io
 import json
-from sklearn.model_selection import train_test_split
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.layers import GlobalAveragePooling2D, Dense
 from tensorflow.keras.models import Model
 from tensorflow.keras.applications.efficientnet import preprocess_input
 from tensorflow.keras.applications.efficientnet import EfficientNetB0
+from tensorflow.keras.optimizers import Adam
+from tqdm import tqdm
+import pickle
+from sklearn.preprocessing import LabelEncoder
+
+
+############## GCP Stuff #######################################################
 
 # Initialize Google Cloud Storage client
 client = storage.Client()
 
 # Define bucket name and paths
 bucket_name = 'lila_channel_islands_data'
-labels_csv_path = 'lila_image_urls_and_labels.csv'
-image_ids_json_path = 'channel_islands_camera_traps.json'
+data_path = 'lila_channel_islands_images_and_labels_temp.csv'
 images_folder = 'cropped_images/'
 
-# Download labels CSV from GCP bucket
-labels_blob = client.bucket(bucket_name).blob(labels_csv_path)
-labels_csv_data = labels_blob.download_as_text()
-labels_df = pd.read_csv(io.StringIO(labels_csv_data))
+# Download data CSV from GCP bucket
+df_blob = client.bucket(bucket_name).blob(data_path)
+data = df_blob.download_as_text()
+df = pd.read_csv(io.StringIO(data))
+num_classes = df['label'].nunique()
 
-# filter labels df to channel islands and extract species and preprocessed image ids
-labels_df = labels_df[labels_df['image_id'].str.startswith(
-    'Channel Islands Camera Traps :')]
-labels_df['id'] = labels_df['image_id'].apply(lambda x: x.split(': ')[1])
-labels_df = labels_df[['id', 'species']]
+############## Data Preprocessing ##############################################
 
-# create image id and file path mappings
-id_blob = client.get_bucket(bucket_name).blob(image_ids_json_path)
-json_data = json.loads(id_blob.download_as_text())
-file_to_id_dict = {entry['file_name']: entry['id']
-                   for entry in json_data['images']}
+# Function to load and preprocess an image
 
-# Download images and preprocess them
-images = []
-labels = []
-for filename in file_to_id_dict.keys():
-    image_blob = client.bucket(bucket_name).blob(images_folder + filename)
-    image_data = image_blob.download_as_bytes()
-    # Decode image with 3 channels (RGB)
-    image = tf.image.decode_image(image_data, channels=3)
-    # Resize image to match EfficientNet input size
-    image = tf.image.resize(image, [224, 224])
-    # Preprocess image using EfficientNet preprocessing function
-    image = preprocess_input(image)
-    images.append(image)
 
-    image_id = file_to_id_dict[filename]
-    label = labels_df[labels_df['id'] == image_id]['species'].values[0]
-    labels.append(label)
+def load_and_preprocess_image(image_path, label):
+    image_blob = client.bucket(bucket_name).blob(
+        images_folder + image_path.numpy().decode('utf-8'))
+    if image_blob.exists():
+        image_data = image_blob.download_as_bytes()
+        image = tf.image.decode_image(image_data, channels=3)
+        image = tf.image.resize(image, [224, 224])
+        image = tf.keras.applications.efficientnet.preprocess_input(image)
+        return image, label
+    else:
+        return None
 
-NUM_CLASSES = labels.nunique()
-# Convert labels to one-hot encoding
-y_data = to_categorical(labels, num_classes=NUM_CLASSES)
 
-# Convert image data to numpy array
-x_data = np.array(images)
+# Create a tf.data.Dataset from labels CSV
+labels_dataset = tf.data.Dataset.from_tensor_slices(
+    (df['image_file'].values, df['label'].values)
+)
+labels_dataset = labels_dataset.map(
+    lambda x, y: tf.py_function(load_and_preprocess_image, [
+                                x, y], [tf.float32, tf.string]),
+    num_parallel_calls=tf.data.AUTOTUNE
+)
 
-# Split data into train and test sets
-x_train, x_test, y_train, y_test = train_test_split(
-    x_data, y_data, test_size=0.2, random_state=42)
+# Filter out None values (images that failed to download)
+labels_dataset = labels_dataset.filter(lambda x, y: x is not None)
+
+# One-hot encode the labels
+
+
+def one_hot_encode(image, label):
+    label = tf.one_hot(label, num_classes)
+    return image, label
+
+
+labels_dataset = labels_dataset.map(
+    lambda x, y: tf.py_function(one_hot_encode, [
+                                x, y], [tf.float32, tf.float32]),
+    num_parallel_calls=tf.data.AUTOTUNE
+)
+
+
+# Batch the dataset and split into train test
+total_samples = len(df)
+labels_dataset = labels_dataset.shuffle(buffer_size=total_samples)
+
+# Define the ratio for splitting the dataset
+train_ratio = 0.6
+val_ratio = 0.2
+test_ratio = 0.2
+train_size = int(total_samples * train_ratio)
+val_size = int(val_ratio * total_samples)
+test_size = int(test_ratio * total_samples)
+
+# Split the dataset into training and testing sets
+train_dataset = labels_dataset.take(train_size)
+val_dataset = labels_dataset.skip(train_size).take(val_size)
+test_dataset = labels_dataset.skip(train_size + val_size)
+
+# Prefetch the dataset for better performance
+batch_size = 16
+train_dataset = train_dataset.batch(
+    batch_size).prefetch(buffer_size=tf.data.AUTOTUNE)
+test_dataset = test_dataset.batch(
+    batch_size).prefetch(buffer_size=tf.data.AUTOTUNE)
+val_dataset = val_dataset.batch(batch_size).prefetch(
+    buffer_size=tf.data.AUTOTUNE)
+
+############## Create Model ####################################################
 
 # Load pre-trained EfficientNetB0 model without top classification layers
 base_model = EfficientNetB0(
@@ -72,17 +113,29 @@ base_model = EfficientNetB0(
 
 # Add custom classification layers
 x = GlobalAveragePooling2D()(base_model.output)
-predictions = Dense(NUM_CLASSES, activation='softmax')(x)
+predictions = Dense(num_classes, activation='softmax')(x)
 model = Model(inputs=base_model.input, outputs=predictions)
 
+# Freeze some layers of the pre-trained model
+for layer in base_model.layers[:-20]:  # Freeze all layers except the last 20
+    layer.trainable = False
+
 # Compile the model
-model.compile(optimizer='adam', loss='categorical_crossentropy',
+model.compile(optimizer=Adam(lr=0.0001), loss='categorical_crossentropy',
               metrics=['accuracy'])
 
-# Fine-tune the model
-model.fit(x_train, y_train, batch_size=32, epochs=10,
-          validation_data=(x_test, y_test))
+############## training ########################################################
 
-# Evaluate the model
-loss, accuracy = model.evaluate(x_test, y_test, batch_size=32)
-print(f'Test accuracy: {accuracy * 100:.2f}%')
+epochs = 10
+history = model.fit(train_dataset, epochs=epochs, validation_data=val_dataset)
+
+# Evaluate the model on the test dataset
+test_loss, test_accuracy = model.evaluate(test_dataset)
+print(f'Test Loss: {test_loss}, Test Accuracy: {test_accuracy}')
+
+############## save model ######################################################
+
+# Upload model to gcp bucket
+pickle_bytes = pickle.dumps(model)
+destination_blob_name = 'lila_channel_islands_efficentNet_test.pkl'
+model_blob = client.bucket(bucket_name).blob(destination_blob_name)
--- src/species-classification/preprocess.py
+++ src/species-classification/preprocess.py
@@ -4,6 +4,7 @@ from google.cloud import storage
 from PIL import Image
 import io
 import json
+from tqdm import tqdm
 
 # Initialize Google Cloud Storage client
 client = storage.Client()
@@ -25,47 +26,49 @@ except json.JSONDecodeError as e:
     exit()
 
 # Process each entry in the JSON file
-for entry in json_data.get('images', []):
+for entry in tqdm(json_data.get('images', []), desc="Processing Images", unit="image"):
     image_path = entry.get('file', '')
     detections = entry.get('detections', [])
 
-    # Check if the image file exists in the bucket
-    if bucket.blob(image_path).exists():
-        image_blob = bucket.blob(image_path)
-        image_data = image_blob.download_as_bytes()
-        image = Image.open(io.BytesIO(image_data))
-        img_w, img_h = image.size
-
-        # Process each detection in the 'detections' list
-        for detection in detections:
-            bbox = detection.get('bbox', [])
-            conf = detection.get('conf', 0)
-            if conf > .90:
-                # Ensure bbox contains valid coordinates
-                if len(bbox) == 4:
-                    # Crop and upload the image
-                    xmin = int(bbox[0] * img_w)
-                    ymin = int(bbox[1] * img_h)
-                    box_w = int(bbox[2] * img_w)
-                    box_h = int(bbox[3] * img_h)
-                    cropped_image = image.crop(
-                        box=[xmin, ymin, xmin + box_w, ymin + box_h])
-
-                    # Upload cropped image to GCS
-                    output_image_path = output_images_folder + \
-                        image_path.split('images/')[1]
-                    output_blob = bucket.blob(output_image_path)
-                    with io.BytesIO() as output_image_data:
-                        # Save as JPG format
-                        cropped_image.save(output_image_data, format='JPEG')
-                        output_image_data.seek(0)
-                        output_blob.upload_from_file(
-                            output_image_data, content_type='image/jpeg')
-
-                    print(f"Cropped image uploaded: {output_image_path}")
-                else:
-                    print(f"Invalid bbox coordinates for image: {image_path}")
-            else:
-                print(f'BBox confidence <= .90: {image_path}')
+    if image_path.startswith('images/'):
+        output_image_path = output_images_folder + \
+            image_path.split('images/')[1]
     else:
-        print(f"Image file not found in the bucket: {image_path}")
+        output_image_path = output_images_folder + image_path
+
+    # Check if image file is already cropped
+    if not bucket.blob(output_image_path).exists():
+        # Check if the image file exists in the bucket
+        if bucket.blob(image_path).exists():
+            image_blob = bucket.blob(image_path)
+            image_data = image_blob.download_as_bytes()
+            try:
+                image = Image.open(io.BytesIO(image_data))
+                img_w, img_h = image.size
+
+                # Process each detection in the 'detections' list
+                for detection in detections:
+                    bbox = detection.get('bbox', [])
+                    conf = detection.get('conf', 0)
+                    if conf > .90:
+                        # Ensure bbox contains valid coordinates
+                        if len(bbox) == 4:
+                            # Crop and upload the image
+                            xmin = int(bbox[0] * img_w)
+                            ymin = int(bbox[1] * img_h)
+                            box_w = int(bbox[2] * img_w)
+                            box_h = int(bbox[3] * img_h)
+                            cropped_image = image.crop(
+                                box=[xmin, ymin, xmin + box_w, ymin + box_h])
+
+                            # Upload cropped image to GCS
+                            output_blob = bucket.blob(output_image_path)
+                            with io.BytesIO() as output_image_data:
+                                # Save as JPG format
+                                cropped_image.save(
+                                    output_image_data, format='JPEG')
+                                output_image_data.seek(0)
+                                output_blob.upload_from_file(
+                                    output_image_data, content_type='image/jpeg')
+            except OSError as e:
+                print(f"Error processing image {image_path}: {e}")
